1e30f57b6e7e43c1673b0daf0e0e0d2de3582a78
tige <tige@sms-group.com>
2018-03-23 10:32:53 +0100

    added converters

---
+++ WPFTest/Helpers/Converters/StringFormatConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPFTest
+{
+    [ValueConversion(typeof(object), typeof(string))]
+    public class StringFormatConverter : BaseConverter, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string format = parameter as string;
+            if (!string.IsNullOrEmpty(format))
+            {
+                return string.Format(culture, format, value);
+            }
+            else
+            {
+                return value.ToString();
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
